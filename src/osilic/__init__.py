16ed910c9a517067340e8907d925cc0f06c4af3d
Dinesh Ravi <dineshr93@gmail.com>
2025-07-14 21:10:33 +0200

    remove the debug

--- src/osilic/__init__.py
+++ src/osilic/__init__.py
@@ -32,7 +32,7 @@ def main() -> None:
                     licenses = license_from_dict(resp.json())
                     for license in licenses:
                         keywords=license.keywords
-                        print(f"Debug: {keywords}")
+                        #print(f"Debug: {keywords}")
                         if len(keywords) > 0:
                             for keyword in keywords:
                                 unique_keywords.add(str(keyword.value))
